52d442672b5a2e4f5b7c74b1c36d698b3d6c26ed
Oxas <Oxas@>
2024-07-06 16:34:30 +0200

    added script to download physics mod (too large file for github repository)

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Mods that can't be added in github repository could be gotten by downloading then using the powershell script "getMods.ps1"
+To run the script, right click on the file and choose "Run with PowerShell"
---
+++ getMods.ps1
@@ -0,0 +1,7 @@
+echo "Downloading mod physics-mod-pro-v170d-fabric-1.20.1.jar"
+curl -o "mods/physics-mod-pro-v170d-fabric-1.20.1.jar" "https://www.patreon.com/file?h=105510754&i=19422292"
+
+
+echo "Mods has been downloaded successfully"
+timeout 5
+del getMods.ps1
