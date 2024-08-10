c5f50aecb0a30603f836aa2dbd9c4f8c18e83af7
WatoLua <32160224+WatoLua@users.noreply.github.com>
2024-08-10 21:35:59 +0200

    script to download better ends mod

---
+++ getMods.ps1
@@ -0,0 +1,7 @@
+echo "Downloading mod better ends 4.0.11"
+curl -o "mods/better-end-4.0.11.jar" "https://www.curseforge.com/api/v1/mods/413596/files/4971487/download"
+
+
+echo "Mods has been downloaded successfully"
+timeout 5
+echo del getMods.ps1
